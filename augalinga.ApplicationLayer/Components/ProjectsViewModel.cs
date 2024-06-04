4161c5c15ddb2dead668d4568b32f48a0c38ff1e
Saulė Abramovičiūtė <abramoviciutesaule@gmail.com>
2024-06-04 15:48:38 +0200

    Basic page layout

---
+++ augalinga.ApplicationLayer/Components/ProjectsViewModel.cs
@@ -0,0 +1,66 @@
+﻿using augalinga.Data.Access;
+using augalinga.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace augalinga.ApplicationLayer.Components
+{
+    public class ProjectsViewModel : INotifyPropertyChanged
+    {
+        public ProjectsViewModel()
+        {
+            LoadProjects();
+        }
+
+        private ObservableCollection<Project> _projects;
+        public ObservableCollection<Project> Projects
+        {
+            get => _projects;
+            set
+            {
+                _projects = value;
+                OnPropertyChanged(nameof(Projects));
+            }
+        }
+
+        public void AddProjectToCollection(Project project)
+        {
+            Projects.Add(project);
+            LoadProjects();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void LoadProjects()
+        {
+            var projects = new DataContext().Projects.ToList();
+            Projects = new ObservableCollection<Project>(projects);
+        }
+
+        public void RemoveProject(string projectName)
+        {
+            //local
+            var projectToRemove = Projects.FirstOrDefault(p => p.Name == projectName);
+            Projects.Remove(projectToRemove);
+
+            //database
+            using (var dbContext = new DataContext())
+            {
+                dbContext.Projects.Remove(projectToRemove);
+                dbContext.SaveChanges();
+            }
+
+            LoadProjects();
+        }
+
+    }
+}
